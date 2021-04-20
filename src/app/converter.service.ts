8ff8124c499faae9e960eab6e04b9d9df2257636
ThomasB <t.bruendl@gmail.com>
2021-04-21 00:03:59 +0200

    first verison of derivate converter

--- src/app/converter.service.ts
+++ src/app/converter.service.ts
@@ -14,6 +14,8 @@ export class ConverterService {
     latexInput = this.cleanLatex(latexInput);
     latexInput = this.fractionConverter(latexInput);
 
+    latexInput = this.derivativeConverter(latexInput);
+
     console.log('BEVORE_NERDAMER__CON_SERV: ', latexInput);
 
     latexInput = this.simplifyLatex(latexInput);
@@ -33,6 +35,9 @@ export class ConverterService {
 
   private cleanLatex(latexInput: string): string {
     while (latexInput.indexOf('\\frac') >= 0) {
+      latexInput = latexInput.replace('\\frac', 'frac');
+    }
+    while (latexInput.indexOf('\\f') >= 0) {
       latexInput = latexInput.replace('\\frac', 'frac');
     }
     latexInput = latexInput.replace(/{/g, '(');
@@ -59,5 +64,100 @@ export class ConverterService {
       }
     }
     return str;
+  }
+
+  private derivativeConverter(str: string): string {
+    console.log('___IN: ', str);
+    let brackedCount = 0;
+    let start: number = str.indexOf('\\deriOne') + 8;
+
+    for (let i = start; i < str.length; i++) {
+      if (str.charAt(i) == '(') {
+        brackedCount++;
+      } else if (str.charAt(i) == ')') {
+        brackedCount--;
+      }
+      if (brackedCount == 0) {
+        console.log(str.substring(start));
+        if (str.substring(start).charAt(0) === '(') {
+          if (str.substring(start + 1, i).indexOf('\\deriOne') >= 0) {
+            console.log('CHILD: ', str.substring(start + 1, i));
+            str =
+              str.substring(0, start) +
+              //   nerdamer('diff(x^2,x)').text('fractions')     -----------------------------------------------------
+              nerdamer(
+                `diff(${this.derivativeConverter(
+                  str.substring(start + 1, i)
+                )},x)`
+              ).text('fractions') +
+              /* math
+                .derivative(
+                  this.derivativeConverter(str.substring(start + 1, i)),
+                  'x',
+                  {
+                    simplify: true,
+                  }
+                )
+                .toString()  */
+              //--------------------------------------------------------------------------------
+
+              str.substring(i + 1, str.length);
+          } else {
+            str =
+              str.substring(0, start) +
+              //   nerdamer('diff(x^2,x)').text('fractions')     -----------------------------------------------------
+              nerdamer(`diff(${str.substring(start + 1, i)},x)`).text(
+                'fractions'
+              ) +
+              /* math
+                .derivative(str.substring(start + 1, i), 'x', {
+                  simplify: true,
+                })
+                .toString() */
+              //--------------------------------------------------------------------------------------------
+
+              str.substring(i + 1, str.length);
+          }
+        }
+
+        //       console.log('Substring: ', str.substring(start, str.charAt(i)));
+
+        str = str.replace('deriOne', ' ');
+        break;
+      }
+    }
+    console.log('___OUT: ', str);
+
+    return str;
+
+    /* let alreadyCalcIndex = 0;
+
+    while (str.indexOf('deriOne') >= alreadyCalcIndex ) {
+
+        
+      let brackedCount = 0;
+      let start: number = str.indexOf('deriOne') + 7;
+      for (let i = start; i < str.length; i++) {
+        if (str.charAt(i) == '(') {
+          brackedCount++;
+        } else if (str.charAt(i) == ')') {
+          brackedCount--;
+        }
+
+        if (brackedCount == 0) {
+          console.log(str.substring(start, brackedCount));
+        }
+        str = str.replace('frac', '');
+        //         if (brackedCount == 0) {
+        //  str = str.slice(0, i + 1) + '/' + str.slice(i + 1);
+        //  str = str.replace('frac', '');
+        //  break;
+       // } 
+        alreadyCalcIndex = start
+
+      }
+    }
+
+    return str; */
   }
 }
